3f5cb53480318add69a41f3ff0661bad97201a73
Brandon <brancarp93@gmail.com>
2022-07-12 17:37:00 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import CharacterList from "./Components/CharacterList";
 import Header from "./Components/Header";
-import SearchBar from "./Components/SearchBar";
-import swloading from './Components/swloading.gif';
+// import SearchBar from "./Components/SearchBar";
+// import swloading from './Components/swloading.gif';
 
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [homeWorld, setHomeWorld] = useState([]);
+  const [species, setSpecies] = useState([]);
   const [nextPageUrl, setNextPageUrl] = useState("https://swapi.dev/api/people/");
   const [backPageUrl, setBackPageUrl] = useState('');
   const [worldUrl, setWorldUrl] = useState([]);
@@ -18,6 +20,7 @@ const App = () => {
     setCharacters(data.results);
     setNextPageUrl(data.next);
     setBackPageUrl(data.previous);
+    return data.results;
   }
 
   const backPage = async () => {
@@ -29,19 +32,43 @@ const App = () => {
 
   const getThings = async () => {
     const {data} = await axios.get(nextPageUrl);
-    return data;
+    return data.results;
   }
 
+  // const getSpecificUrl = async (url) => {
+  //   const {data} = await axios.get(url);
+  //   console.log(data);
+  //   return data;
+  // }
 
+ 
+
+  // useEffect(() => {
+  //   async function getSwapi() {
+  //     const things = await getThings();
+  //     const specificUrl = await Promise.all([
+  //       things.map(thing => axios.get(thing.homeworld)
+  //       )
+  //     ]);
+  //       setHomeWorld(Promise.resolve(specificUrl));
+  //   }
+  //    getSwapi();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  
   useEffect(() => {
-    fetchPeople() 
-    async function getStuff() {
-      const things = await getThings();
-      
+    async function getSwapi() {
+      const things = await fetchPeople();
+      const specificUrl = await Promise.all(
+        things.map((thing) => axios.get(thing.homeworld))
+      );
+      setHomeWorld(specificUrl);
     }
-     getStuff();
+     getSwapi();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  
   
   
 
@@ -49,13 +76,13 @@ const App = () => {
  
   return (
     <div>
-     <Header />
+     {/* <Header /> */}
       <CharacterList  list={characters}/>
      <h4>Real Buttons Below</h4>
      <button onClick={(e) => backPage()}>Back Page</button>
      <button onClick={(e) => fetchPeople()}>Next Page</button>
      <h3>Test Button</h3>
-     <button onClick={console.log()}>Test</button>
+     <button onClick={(e) => console.log(characters)}>Test</button>
     </div>
   );
 }
