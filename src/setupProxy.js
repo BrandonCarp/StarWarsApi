7bc54ce088a0cc5759a75d2ce0f6c1a846319dbd
Brandon C <brancarp93@gmail.com>
2023-01-10 17:37:33 -0500

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "https://swapi.dev",
     createProxyMiddleware({
-      target: "https://localhost:7002",
+      target: "https://starwarsreactapi.netlify.app",
       changeOrigin: true,
       secure: false,
     })
